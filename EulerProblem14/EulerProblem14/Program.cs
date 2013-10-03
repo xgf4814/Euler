ca0e258730ae033d90cac5df2a100ee13ccde063
Xander Fiss <xandfis@microsoft.com>
2013-10-03 14:56:21 -0700

    Euler Problem 14
    The following iterative sequence is defined for the set of positive
    integers:
    
    n → n/2 (n is even)
    n → 3n + 1 (n is odd)
    
    Using the rule above and starting with 13, we generate the following
    sequence:
    
    13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
    It can be seen that this sequence (starting at 13 and finishing at 1)
    contains 10 terms. Although it has not been proved yet (Collatz
    Problem), it is thought that all starting numbers finish at 1.
    
    Which starting number, under one million, produces the longest chain?
    
    NOTE: Once the chain starts the terms are allowed to go above one
    million.

---
+++ EulerProblem14/EulerProblem14/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProblem14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            long n;
+            int counter;
+            int longest = 0;
+            int longest_n = 0;
+            for (int i = 1; i < 1000000; i++)
+            {
+                Console.Write("\r{0}   ", i);
+                counter = 0;
+                n = i;
+                while (n != 1)
+                {
+                    if (n % 2 == 0)
+                    {
+                        n /= 2;
+                    }
+                    else
+                    {
+                        n = 3 * n + 1;
+                    }
+                    counter++;
+                }
+                if (counter > longest)
+                {
+                    longest = counter;
+                    longest_n = i;
+                }
+            }
+            Console.WriteLine("Longest, n = " + longest_n);
+            Console.WriteLine("Chain Length = " + longest);
+
+        }
+    }
+}
