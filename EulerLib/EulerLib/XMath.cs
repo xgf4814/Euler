2d21829388b1c1cc7b6887fd492a763518363e52
Xander Fiss <xanderfiss@gmail.com>
2013-10-05 20:36:54 -0700

    Euler Problem 21
    Let d(n) be defined as the sum of proper divisors of n (numbers less
    than n which divide evenly into n).
    If d(a) = b and d(b) = a, where a ? b, then a and b are an amicable pair
    and each of a and b are called amicable numbers.
    
    For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22,
    44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are
    1, 2, 4, 71 and 142; so d(284) = 220.
    
    Evaluate the sum of all the amicable numbers under 10000.

---
+++ EulerLib/EulerLib/XMath.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerLib
+{
+    public class XMath
+    {
+        public static List<int> ProperDivisors(int n)
+        {
+
+            List<int> divisors = new List<int>();
+
+            if (n == 1)
+                throw new NotImplementedException();
+
+            // Always add one as a special case, since we aren't supposed to add n
+            divisors.Add(1);
+
+            if (n == 2 || n == 3)
+                return divisors;
+
+            int intsqrt = (int)Math.Sqrt(n);
+
+            for (int i = 2; i < intsqrt; i++)
+            {
+                if (n % i == 0)
+                {
+                    divisors.Add(i);
+                    divisors.Add(n / i);
+                }
+            }
+
+            // square root case
+            if (n % intsqrt == 0)
+                divisors.Add(intsqrt);
+
+            return divisors;
+        }
+    }
+}
---
+++ EulerProblem21/EulerProblem21/Program.cs
@@ -0,0 +1,97 @@
+﻿using EulerLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProblem21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int limit = 0;
+
+            if (args.Length == 1)
+            {
+                try
+                {
+                    limit = int.Parse(args[0]);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Specify the upper limit");
+                return;
+            }
+
+            bool debug = false;
+
+            if (debug)
+            {
+                List<int> foo = XMath.ProperDivisors(limit);
+                foo.Sort();
+                foreach (int i in foo)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine();
+                Console.WriteLine(d(limit));
+                Console.WriteLine(IsAmicable(limit));
+            }
+            else
+            {
+                int sum = 0;
+                for (int i = 2; i < limit; i++)
+                {
+                    if (IsAmicable(i))
+                    {
+                        sum += i;
+                    }
+                }
+
+                Console.WriteLine(sum);
+            }
+
+        }
+
+        static bool IsAmicable(int a)
+        {
+            if (a == 0)
+                return false;
+            
+            int x = d(a);
+
+            if (x < 2)
+                return false;
+
+            int y = d(x);
+
+            if (a == y && a != x)
+            {
+                Console.WriteLine("Amicable Pair: " + a + " " + x);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static int d(int n)
+        {   
+            int sum = 0;
+            foreach (int i in XMath.ProperDivisors(n))
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+    }
+}
