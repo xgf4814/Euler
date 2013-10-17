5cea6a487b61fd8120af032d49a5eaa020ec13f7
Xander Fiss <xandfis@microsoft.com>
2013-10-17 10:03:53 -0700

    Euler Problem 25: 1000-digit Fibonacci number
    The Fibonacci sequence is defined by the recurrence relation:
    
    Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
    Hence the first 12 terms will be:
    
    F1 = 1
    F2 = 1
    F3 = 2
    F4 = 3
    F5 = 5
    F6 = 8
    F7 = 13
    F8 = 21
    F9 = 34
    F10 = 55
    F11 = 89
    F12 = 144
    The 12th term, F12, is the first term to contain three digits.
    
    What is the first term in the Fibonacci sequence to contain 1000 digits?

---
+++ EulerProblem25/EulerProblem25/Program.cs
@@ -0,0 +1,31 @@
+﻿using EulerLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProblem25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string i = "1";
+            string j = "1";
+            string k = "1";
+
+            int term = 2;
+            while (k.Length < int.Parse(args[0]))
+            {
+                k = EulerLib.StringInt.Add(i, j);
+                i = j;
+                j = k;
+                term++;
+                Console.WriteLine("F" + term + " = " + k);
+            }
+
+        }
+    }
+}
