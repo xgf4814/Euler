c4b5f644ebd842fb9d1abaca433363fae88598e9
Xander Fiss <xandfis@microsoft.com>
2013-10-17 10:59:05 -0700

    Euler Problem 26: Reciprocal cycles
    A unit fraction contains 1 in the numerator. The decimal representation
    of the unit fractions with denominators 2 to 10 are given:
    
    1/2 =  0.5
    1/3 =  0.(3)
    1/4 =  0.25
    1/5 =  0.2
    1/6 =  0.1(6)
    1/7 =  0.(142857)
    1/8 =  0.125
    1/9 =  0.(1)
    1/10 =  0.1
    Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It
    can be seen that 1/7 has a 6-digit recurring cycle.
    
    Find the value of d < 1000 for which 1/d contains the longest recurring
    cycle in its decimal fraction part.

---
+++ EulerProblem26/EulerProblem26/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProblem26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Implement Long Division
+            int max_divisor = int.Parse(args[0])-1;
+            int d_longestrecurring = 3;
+            int longest_recurring_cycle = 1;
+
+            int x;
+            int y;
+            int r;
+            List<int> previous = new List<int>();
+            //for (int d = max_divisor+1; d <= max_divisor+1; d++)
+            for (int d = 2; d <= max_divisor; d++)
+            {
+                previous.Clear();
+                x = 1;
+                r = 0;
+
+                // Long Division
+                while(!previous.Contains(x) && x != 0)
+                {
+                    previous.Add(x);
+                    while (d > x)
+                        x *= 10;
+                    y = d * (x / d);
+
+                    x = x - y;                  
+                    r++;
+                }
+
+                //Console.WriteLine("1/" + d + "; " + r + "; " + x);
+
+                // Determine if repeating
+                r = r - previous.IndexOf(x);
+                if (x != 0)
+                {
+                    if (r > longest_recurring_cycle)
+                    {
+                        longest_recurring_cycle = r;
+                        d_longestrecurring = d;
+                    }
+                }
+
+            }
+
+            Console.WriteLine("\nDivisor = " + d_longestrecurring);
+            Console.WriteLine("Length of Recurrence = " + longest_recurring_cycle);
+            Console.WriteLine((1.0 / (double)d_longestrecurring));
+
+        }
+    }
+}
