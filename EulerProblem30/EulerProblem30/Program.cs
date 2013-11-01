ab4943fd4c3a36833b0f39f85faa4d5478bc4a45
Xander Fiss <xandfis@microsoft.com>
2013-11-01 12:44:30 -0700

    Euler Problem 30: Digit fifth powers
    Surprisingly there are only three numbers that can be written as the sum
    of fourth powers of their digits:
    
    1634 = 1^4 + 6^4 + 3^4 + 4^4
    8208 = 8^4 + 2^4 + 0^4 + 8^4
    9474 = 9^4 + 4^4 + 7^4 + 4^4
    As 1 = 1^4 is not a sum it is not included.
    
    The sum of these numbers is 1634 + 8208 + 9474 = 19316.
    
    Find the sum of all the numbers that can be written as the sum of fifth
    powers of their digits.

---
+++ EulerProblem30/EulerProblem30/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProblem30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> sums = new List<int>();
+            string s = "";
+            int sum;
+            for (int x = 2; x <= 236196; x++) //236196 for 5, 26244 for 4
+            {
+                sum = 0;
+                s = x.ToString();
+                
+                for(int i = 0; i < s.Length; i++)
+                {
+                    sum += (int)Math.Pow((double)int.Parse(s.Substring(i, 1)), (double)5);
+                }
+
+                if (sum == x)
+                {
+                    Console.WriteLine(x);
+                    sums.Add(x);
+                }    
+            }
+
+            sum = 0;
+            foreach(int x in sums)
+            {
+                sum += x;
+            }
+            Console.WriteLine("Sum Total = " + sum);
+
+        }
+    }
+}
