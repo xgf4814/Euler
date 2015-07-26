02c16b62d41b1119efb5f1eca949c8d874409f36
xgf4814 <xander@jolteon.local>
2015-07-26 13:25:09 -0700

    Adding a template for new problems

---
+++ Python/ProblemTemplate.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python -tt
+
+"""
+Project Euler: Problem 3
+"""
+
+import sys
+
+# Define a main() function
+def main():
+  #print "This is the name of the script: ", sys.argv[0]
+  #print "Number of arguments: ", len(sys.argv)
+  #print "The arguments are: " , str(sys.argv)
+  
+
+# This is the standard boilerplate that calls the main() function.
+if __name__ == '__main__':
+  main()
