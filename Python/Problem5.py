02aeff83e237044050159ff7afa5eb628150f567
xgf4814 <xander@jolteon.local>
2015-07-27 13:58:42 -0700

    Euler Problem 5 in Python

--- Python/Problem5.py
+++ Python/Problem5.py
@@ -6,13 +6,24 @@ Project Euler: Problem 5
 
 import sys
 
-# Define a main() function
-def main():
-  #print "This is the name of the script: ", sys.argv[0]
-  #print "Number of arguments: ", len(sys.argv)
-  #print "The arguments are: " , str(sys.argv)
-  
+def divisible_by_range(target, divisors):
+  for d in divisors:
+    if target % d != 0:
+      return False
+  return True
 
+# Define a main() function
+def main():  
+  limit = int(sys.argv[1]) + 1
+  divisors = range(1,limit)
+  i = 1
+  while True:
+    if divisible_by_range(i,divisors):
+      print i
+      return
+    else:
+      i += 1
+  
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
   main()
